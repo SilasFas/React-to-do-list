2f2286e6020c0571ffa3123e50be7777a5c1ad04
Silas <silasfas@hotmail.com>
2023-03-23 21:37:13 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask])
+  }
+
+  const deleteTask = () => {
+    setTodoList()
+  }
+
   return (
     <div className="App">
-      <h1>Start here!</h1>
+
+      <div className='addTask'>
+        <input
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+        <button onClick={addTask}>Add task</button>
+      </div>
+
+      <div className='List'>
+
+        {todoList.map((task, key) => {
+          return (
+            <div key={key}>
+              <h1> {task} </h1>
+              <button onClick={() => deleteTask(task)}>x</button>
+            </div>
+          )
+
+
+
+
+        })}
+
+      </div>
+
     </div>
   );
 }
-
 export default App;
